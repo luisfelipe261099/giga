9b08369334b3660b33d3c5e565b75f27a489f369
Luis Felipe Da Silva Machado <166923042+luisfelipe261099@users.noreply.github.com>
2026-04-29 11:17:27 -0300

    ajute

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        <link rel="stylesheet" href="/site.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
       </head>
       <body className={`${inter.variable} ${poppins.variable}`}>
